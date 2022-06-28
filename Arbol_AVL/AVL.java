cae34b5a6e2098e752a0328354adb566e15e6aee
Edson López <elopezqu@unsa.edu.pe>
2022-06-28 14:41:45 -0500

    Se toma en consideración el factor de equilibrio y se utiliza las rotaciones, tambien se eliminan métodos

--- Arbol_AVL/AVL.java
+++ Arbol_AVL/AVL.java
@@ -1,53 +1,104 @@
 public class AVL<T extends Comparable<T>> {
 
     protected NodeAVL<T> root;
+    protected boolean altura;
 
     public AVL() { this.root = null; }
 
     public void insert(T x) {
+        this.altura = false;
         this.root = insertRec(x, this.root);
     }
     private NodeAVL<T> insertRec(T x, NodeAVL<T> current){
         NodeAVL<T> res = current;
         if(current == null){
+            altura = true;
             res = new NodeAVL<T>(x);
         }else{
             int resC = current.data.compareTo(x);
             if(resC < 0){
                 res.right = insertRec(x,(NodeAVL<T>)current.right);
+                if(this.altura){
+                    switch(res.fe){
+                        case -1 : res.fe = 0; this.altura = false; break;
+                        case 0  : res.fe = 1; this.altura = true; break;
+                        case 1  : //res.fb = 2
+                                  res = balanceToLeft(res);
+                                  this.altura = false;
+                    }
+                }
             }
             else{
                 res.left = insertRec(x,(NodeAVL<T>)current.left);
+                if(this.altura){
+                    switch(res.fe){
+                        case 1 : res.fe = 0; this.altura = false; break;
+                        case 0  : res.fe = -1; this.altura = true; break;
+                        case -1  : //res.fb = 2
+                                  res = balanceToRight(res);
+                                  this.altura = false;
+                    }
+                }
             }
         }
         return res;
     }
-
-    public void remove(T x) {
-    };
-
     protected NodeAVL<T> rotateSR(NodeAVL<T> node) {
         NodeAVL<T> h = (NodeAVL<T>) node.left;
         node.left = h.right;
         h.right = node;
         node = h;
-
-        updateHeight(node);
-        updateHeight(h);
         return node;
     }
-
     protected NodeAVL<T> rotateSL(NodeAVL<T> node) {
         NodeAVL<T> h = (NodeAVL<T>) node.right;
         node.right = h.left;
         h.left = node;
         node = h;
-
-        updateHeight(node);
-        updateHeight(h);
-
         return node;
     }
+    private NodeAVL<T> balanceToLeft(NodeAVL<T> node){
+        NodeAVL<T> ind = (NodeAVL<T>)node.right;
+        switch(ind.fe){
+            case 1  : node.fe = 0;
+                      ind.fe = 0;
+                      node = rotateSL(node); 
+                      break;
+            case -1 : NodeAVL<T> grandInd= (NodeAVL<T>)ind.left;
+                      switch(grandInd.fe){
+                        case -1 : node.fe = 0; ind.fe = -1; break;
+                        case 0  : node.fe = 0; ind.fe = 0; break;
+                        case 1  : node.fe = 1; ind.fe = 0; break;
+                      }
+                      grandInd.fe = 0;
+                      node.right = rotateSR(ind);
+                      node = rotateSL(node);
+        }
+        return node;
+    }
+    private NodeAVL<T> balanceToRight(NodeAVL<T> node){
+        NodeAVL<T> ind = (NodeAVL<T>)node.left;
+        switch(ind.fe){
+            case 1  : node.fe = 0;
+                      ind.fe = 0;
+                      node = rotateSR(node); 
+                      break;
+            case -1 : NodeAVL<T> grandInd= (NodeAVL<T>)ind.right;
+                      switch(grandInd.fe){
+                        case -1 : node.fe = 0; ind.fe = 1; break;
+                        case 0  : node.fe = 0; ind.fe = 0; break;
+                        case 1  : node.fe = -1; ind.fe = 0; break;
+                      }
+                      grandInd.fe = 0;
+                      node.right = rotateSL(ind);
+                      node = rotateSR(node);
+        }
+        return node;
+    }
+    
+    public void remove(T x) {
+    };
+
 
     protected NodeAVL<T> rotateDSR(NodeAVL<T> node) {
         node.left = rotateSL((NodeAVL<T>) node.right);
@@ -90,7 +141,7 @@ public class AVL<T extends Comparable<T>> {
             str += postOrden((NodeAVL<T>)current.left);
         if (current.right != null)
             str += postOrden((NodeAVL<T>)current.right);
-        str += current.data.toString() + ", ";
+            str += current.data + "[" + current.fe +"], ";
         return str;
     }
 
--- Arbol_AVL/Test.java
+++ Arbol_AVL/Test.java
@@ -1,11 +1,15 @@
 public class Test {
     public static void main(String[] args){
         AVL<Integer> arbol = new AVL<Integer>();
-        arbol.insert(5);
-        System.out.println(arbol);
         arbol.insert(6);
         System.out.println(arbol);
         arbol.insert(7);
+        System.out.println(arbol);
+        arbol.insert(1);
+        System.out.println(arbol);
+        arbol.insert(3);
+        System.out.println(arbol);
+        arbol.insert(2);
         System.out.println(arbol);
     }
 }
