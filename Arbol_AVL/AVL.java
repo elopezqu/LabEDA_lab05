4db6132eded4e930e249e576c1bede15575d8d67
diego pacori <dpacoria@unsa.edu.pe>
2022-06-28 00:57:12 -0500

    Se hizo un avance de la clase AVL, aun faltan algunos metodos

---
+++ Arbol_AVL/AVL.java
@@ -0,0 +1,57 @@
+public class AVL<T extends Comparable<T>> {
+
+    protected NodeAVL<T> root;
+
+    public AVL() { this.root = null; }
+
+    public void insert(T x){};
+
+    public void remove(T x){};
+    
+    
+    protected NodeAVL<T> rotateSR(NodeAVL<T>node) {
+        NodeAVL<T>h = (NodeAVL)node.left;
+        node.left = h.right;
+        h.right = node;
+        node = h;
+        
+        updateHeight(node);
+        updateHeight(h);
+        return node;
+    }
+    protected NodeAVL<T> rotateSL(NodeAVL<T>node) {
+        NodeAVL<T>h = (NodeAVL)node.right;
+        node.right = h.left;
+        h.left = node;
+        node = h;
+        
+        updateHeight(node);
+        updateHeight(h);
+        
+        return node;
+    }
+    protected NodeAVL<T> rotateDSR(NodeAVL<T>node){
+        node.left = rotateSL((NodeAVL<T>)node.getLeftNode());
+        return rotateSR(node);
+   
+    }
+    protected NodeAVL<T> rotateDSL(NodeAVL<T>node){
+        node.right = rotateSR((NodeAVL<T>)node.getLeftNode());
+        return rotateSL(node);
+    }
+    private void updateHeight(NodeAVL<T> node){
+        int leftHeight = height((NodeAVL<T>)node.left);
+        int rightHeight = height((NodeAVL<T>)node.right);
+        
+        node.setFE( Math.max(leftHeight, leftHeight)+1 );
+    }
+    private int height(NodeAVL<T> node){
+        if(node == null)
+            return -1;
+        return node.fe;
+    }
+    private int factorEquilibrio(NodeAVL<T> node){
+        return height((NodeAVL<T>)node.right) - height((NodeAVL<T>)node.left);
+    }
+    
+}
